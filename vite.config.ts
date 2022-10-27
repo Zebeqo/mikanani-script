a003a0e3d30579f48ef80ece6483de94b5f05291
Zebeqo <z715596546@qq.com>
2022-10-27 17:17:09 +0800

    chore: add userscript headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://mikanani.me/Home/Bangumi/*'],
+        version: "1.0.0",
+        icon: "https://mikanani.me/images/favicon.ico",
+        namespace: "https://github.com/Zebeqo/",
+        match: ["https://mikanani.me/Home/Bangumi/*"],
+        name: "蜜柑计划|mikanani|Mikan Project|批量复制磁力链接",
+        author: "Zebeqo",
+        description:
+          "为详情页，如https://mikanani.me/Home/Bangumi/2841，提供复选框批量复制磁力链接功能",
+        updateURL:
+          "https://raw.githubusercontent.com/Zebeqo/mikanani-script/master/dist/mikanani-script.user.js",
+        downloadURL:
+          "https://raw.githubusercontent.com/Zebeqo/mikanani-script/master/dist/mikanani-script.user.js",
+        supportURL: "https://github.com/Zebeqo/mikanani-script/issues",
+        $extra: [
+          ["name:en", "mikanani|Mikan Project|Multi-selector"],
+          ["name:zh", "蜜柑计划|mikanani|Mikan Project|批量复制磁力链接"],
+          [
+            "description:en",
+            "For detail pages, such as https://mikanani.me/Home/Bangumi/2841, provide checkboxes to batch copy magnet links.",
+          ],
+        ],
       },
     }),
   ],
